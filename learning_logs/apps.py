b0a6f52e72856cd9b2126fe9c1a69d94b3f77542
xwp <xwp_fullstack@163.com>
2018-05-10 17:07:14 +0800

    fe

--- learning_logs/apps.py
+++ learning_logs/apps.py
@@ -3,3 +3,22 @@ from django.apps import AppConfig
 
 class LearningLogsConfig(AppConfig):
     name = 'learning_logs'
+
+import web
+
+urls = (
+    '/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+application = app.wsgifunc()
+
+class hello:
+    def GET(self, name):
+        if not name:
+            name = 'World'
+        return 'Hello, ' + name + '!'
+
+if __name__ == "__main__":
+    app.run()
+
+
