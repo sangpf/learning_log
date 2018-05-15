c3421769c6b3d568f9f715177f497fdc22ddc9a8
xwp <xwp_fullstack@163.com>
2018-05-15 13:40:24 +0800

    fe

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'learning_log',
+    #     'USER': 'root',
+    #     'PASSWORD': 'sang@123456',
+    #     'HOST': '47.94.108.128',
+    #     'PORT': '3306',
+    # }
 }
 
 
